aeacbc8ba1af654a3641bba0dc86e80a9be796dc
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-03-13 23:12:12 +0530

    changes

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  "https://uqpgzjvgboztmoranlng.supabase.co",
+  "https://ndxjhhgwpydyiayqbkdr.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keGpoaGd3cHlkeWlheXFia2RyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3Nzk1MTg2MSwiZXhwIjoxOTkzNTI3ODYxfQ.NWlys9JAUpbYvMfIy-PHYJYgNHZJdMzpdmkDRJv7WFc"
 );
 
