4dba1470134f6d7d9f72bca6b20b51432782b2f4
Megha-Dev-19 <meghadev1901@gmail.com>
2023-07-11 22:32:09 +0530

    updated api

--- router/scoreboard.js
+++ router/scoreboard.js
@@ -3,31 +3,19 @@ const Router = new express.Router();
 const { supabase } = require("../utils/supabase");
 const table = "scoreboard"
 
-Router.get("/scoreboard/community/:communityName/:communityVertical", async (req, res) => {
-    const communityName = req.params.communityName;
-    const communityVertical = req.params.communityVertical;
-    if (communityName && communityVertical) {
-    const conditions = {
-        'community-name' : communityName,
-        'community-vertical' : communityVertical
+Router.get("/scoreboard", async (req, res) => {
+    const {communityName, communityVertical} = req.query;
+    if (communityName || communityVertical) {
+    const conditions = {}
+    if (communityName) {
+        conditions['community-name'] = communityName
+    }
+    if (communityVertical) {
+     conditions['community-vertical'] = communityVertical
     }
     const { error, data } = await supabase.from(table).select("*").match(conditions);
     res.send({ error, data });
-    }
-});
-
-Router.get("/scoreboard/communityName/:name", async (req, res) => {
-    const communityName = req.params.name;
-    const conditions = {'community-name' : communityName}
-    const { error, data } = await supabase.from(table).select("*").match(conditions);
-    res.send({ error, data });    
-});
-
-Router.get("/scoreboard/communityVertical/:vertical", async (req, res) => {
-    const communityVertical = req.params.vertical;
-    const conditions = {'community-vertical' : communityVertical}
-    const { error, data } = await supabase.from(table).select("*").match(conditions);
-    res.send({ error, data });
+    } else res.send({ error: 'Please add community name or vertical.', data:[] });
 });
 
 module.exports = Router;
