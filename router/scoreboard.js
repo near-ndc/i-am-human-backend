64d434115dc551fc30eafc962352d27a13a31c93
Megha-Dev-19 <meghadev1901@gmail.com>
2023-09-01 13:47:46 +0530

    update scoreboard API to send all results

--- router/scoreboard.js
+++ router/scoreboard.js
@@ -15,7 +15,35 @@ Router.get("/scoreboard", async (req, res) => {
     }
     const { error, data } = await supabase.from(table).select("*").match(conditions);
     res.send({ error, data });
-    } else res.send({ error: 'Please add community name or vertical.', data:[] });
+    } else {
+    // the result can be > 1000, so need pagination
+    let errorMessage = ""
+    let pageIndex = 0;
+    let pageSize = 1000; // Number of records to fetch per page
+    let totalResults = [];
+    let runCode = true;
+    while (runCode) {
+      const { error, data } = await supabase.from(table).select('*')
+      .range(pageIndex * pageSize, (pageIndex + 1) * pageSize - 1);
+
+      if (error) {
+        errorMessage = error
+        runCode = false;
+        break;
+      }
+
+      if (data.length === 0) {
+        runCode = false;
+        // No more results, break the loop
+        break;
+      }
+
+      totalResults = totalResults.concat(data);
+      pageIndex++;
+    }
+    await Promise.all(totalResults);
+    res.send({ error:errorMessage, data:totalResults });
+    }
 });
 
 module.exports = Router;
