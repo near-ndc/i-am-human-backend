641f5b550a18dd1587fe919a60d98c86791834c5
Megha-Dev-19 <meghadev1901@gmail.com>
2023-09-13 21:26:01 +0530

    update scoreboard API for blacklist users

--- router/scoreboard.js
+++ router/scoreboard.js
@@ -1,33 +1,76 @@
-const express = require("express");
+const express = require('express');
 const Router = new express.Router();
-const { supabase } = require("../utils/supabase");
-const table = "scoreboard"
+const { supabase } = require('../utils/supabase');
+const table = 'scoreboard';
+const fs = require('fs');
+const { parse } = require('csv-parse');
 
-Router.get("/scoreboard", async (req, res) => {
-    const {communityName, communityVertical} = req.query;
-    if (communityName || communityVertical) {
-    const conditions = {}
+async function loadCSV(filename, ignore) {
+  try {
+    const data = [];
+
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(filename)
+        .pipe(
+          parse({
+            delimiter: ',',
+            columns: true,
+            ltrim: true,
+          })
+        )
+        .on('data', function (row) {
+          data.push(row);
+        })
+        .on('error', function (error) {
+          console.error('Error:', error.message);
+          reject(error);
+        })
+        .on('end', function () {
+          resolve(data);
+        });
+    });
+  } catch (e) {
+    if (!ignore) {
+      console.error('Failed to load CSV:', filename, e);
+    }
+  }
+  return null;
+}
+
+Router.get('/scoreboard', async (req, res) => {
+  const blacklistArray = await loadCSV('./blacklist.csv');
+  const { communityName, communityVertical } = req.query;
+  if (communityName || communityVertical) {
+    const conditions = {};
     if (communityName) {
-        conditions['community-name'] = communityName
+      conditions['community-name'] = communityName;
     }
     if (communityVertical) {
-     conditions['community-vertical'] = communityVertical
+      conditions['community-vertical'] = communityVertical;
     }
-    const { error, data } = await supabase.from(table).select("*").match(conditions);
+    let { error, data } = await supabase
+      .from(table)
+      .select('*')
+      .match(conditions);
+    data = data?.filter((item) => {
+      return !blacklistArray?.find((i) => i.owner_id === item.account);
+    });
     res.send({ error, data });
-    } else {
+  } else {
     // the result can be > 1000, so need pagination
-    let errorMessage = ""
+    let errorMessage = '';
     let pageIndex = 0;
     let pageSize = 1000; // Number of records to fetch per page
     let totalResults = [];
     let runCode = true;
     while (runCode) {
-      const { error, data } = await supabase.from(table).select('*')
-      .range(pageIndex * pageSize, (pageIndex + 1) * pageSize - 1);
+      const { error, data } = await supabase
+        .from(table)
+        .select('*')
+        .range(pageIndex * pageSize, (pageIndex + 1) * pageSize - 1);
 
       if (error) {
-        errorMessage = error
+        errorMessage = error;
         runCode = false;
         break;
       }
@@ -42,8 +85,11 @@ Router.get("/scoreboard", async (req, res) => {
       pageIndex++;
     }
     await Promise.all(totalResults);
-    res.send({ error:errorMessage, data:totalResults });
-    }
+    totalResults = totalResults?.filter((item) => {
+      return !blacklistArray?.find((i) => i.owner_id === item.account);
+    });
+    res.send({ error: errorMessage, data: totalResults });
+  }
 });
 
 module.exports = Router;
